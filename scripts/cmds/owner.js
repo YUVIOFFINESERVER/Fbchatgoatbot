0d746c162984860ab1086daa156111ab58436d8b
ⲘᏒ᭄Sʌʜɩɭ <140341442+S9HIL@users.noreply.github.com>
2025-09-27 20:57:17 +0530

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -17,7 +17,7 @@ module.exports = {
   onStart: async function ({ api, event }) {
     try {
       const ownerInfo = {
-        name: '𒁍 ⟬ 𓆩YUVi ‣⃟ ⃝𑁍𓆪᭄ 達 ⟭ ꪹ 爾 ᯽⸺›⁐‡𖣴‣ ⸨⸙⸩',
+        name: '𒁍 ⟬ 𓆩Yuvi ‣⃟ ⃝𑁍𓆪᭄ 達 ⟭ ꪹ 爾 ᯽⸺›⁐‡𖣴‣ ⸨⸙⸩',
         gender: '𝗠𝗮𝗹𝗲',
         favourite: 'Favourite emoji 🙂👈',
         Fb: 'https://www.facebook.com/profile.php?id=100056840426152',
@@ -25,17 +25,17 @@ module.exports = {
         bio: '𝗗𝗶𝘀𝗰𝗼𝗻𝗻𝗲𝗰𝘁 𝗺𝗲 𝗳𝗿𝗼𝗺 𝘁𝗵𝗲 𝘄𝗼𝗿𝗹𝗱 𝗼𝗳 𝘁𝗲𝗰𝗵𝗻𝗼𝗹𝗼𝗴𝘆 𝗮𝗻𝗱 𝗶 𝘄𝗶𝗹𝗹 𝗯𝗲 𝘆𝗼𝘂𝗿 𝗴𝘂𝗶𝗱𝗲. 𝗜 𝘄𝗶𝗹𝗹 𝗵𝗲𝗹𝗽 𝘆𝗼𝘂 𝘁𝗼 𝗹𝗲𝗮𝗿𝗻 𝗮𝗻𝗱 𝗴𝗿𝗼𝘄. 𝗜 𝘄𝗶𝗹𝗹 𝗯𝗲 𝘆𝗼𝘂𝗿 𝗳𝗿𝗶𝗲𝗻𝗱 𝗮𝗻𝗱 𝗴𝘂𝗶𝗱𝗲 𝘆𝗼𝘂 𝘁𝗵𝗿𝗼𝘂𝗴𝗵 𝘁𝗵𝗲 𝗷𝗼𝘂𝗿𝗻𝗲𝘆 𝗼𝗳 𝗰𝗼𝗱𝗶𝗻𝗴'
       };
 
-      // yaha pe direct image link dalna h (not ibb.co page link)
-      const imageUrl = 'https://ibb.co/fY4CrHRh';  
-
+      const bold = 'https://streamable.com/rmv98c';
       const tmpFolderPath = path.join(__dirname, 'tmp');
+
       if (!fs.existsSync(tmpFolderPath)) {
         fs.mkdirSync(tmpFolderPath);
       }
 
-      const imgResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-      const imgPath = path.join(tmpFolderPath, 'owner_image.jpg');
-      fs.writeFileSync(imgPath, Buffer.from(imgResponse.data, 'binary'));
+      const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
+      const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
+
+      fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
 
       const response = `
 ◈ 𝖮𝖶𝖭𝖤𝖱 𝖨𝖭𝖥𝖮𝖱𝖬𝖠𝖳𝖨𝖮𝖭:
@@ -43,17 +43,17 @@ module.exports = {
 Name: ${ownerInfo.name}
 Gender: ${ownerInfo.gender}
 Relationship: ${ownerInfo.Relationship}
-Favourite: ${ownerInfo.favourite}
+Hobby: ${ownerInfo.hobby}
 Fb: ${ownerInfo.Fb}
 Bio: ${ownerInfo.bio}
       `;
 
       await api.sendMessage({
         body: response,
-        attachment: fs.createReadStream(imgPath)
+        attachment: fs.createReadStream(videoPath)
       }, event.threadID, event.messageID);
 
-      fs.unlinkSync(imgPath);
+      fs.unlinkSync(videoPath);
 
       api.setMessageReaction('🚀', event.messageID, (err) => {}, true);
     } catch (error) {
